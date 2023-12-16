c1914e477738b3968c00756e6b34c4afca7ab898
M.A.Sasindu Mallawarachchi <86243528+SasinduMallawarachchi@users.noreply.github.com>
2023-12-16 20:24:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ rotateOptions.forEach(option => {
         applyFilter();
     });
 });
+
 const resetFilter = () => {
     brightness = "100"; saturation = "100"; inversion = "0"; grayscale = "0";
     rotate = 0; flipHorizontal = 1; flipVertical = 1;
